b145d2a95be5d173e2be5ca692742bf6cd791e41
Deepak_Jha <deepakjha1104@gmail.com>
2023-09-17 23:32:16 +0530

    newest

--- src/Context/notes/NoteState.js
+++ src/Context/notes/NoteState.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import NoteContext from "./NoteContext";
 
 const NoteState = (props) => {
-  const host = "https://mern-notebook-2.onrender.com/"
+  const host = "https://notebook-eput.onrender.com"
   const notesInitial = [];
   const [notes, setNotes] = useState(notesInitial)
 
