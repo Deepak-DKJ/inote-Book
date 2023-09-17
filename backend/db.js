f6a1644303af8fb2b49ce0385aec3e74f6e5f622
Deepak_Jha <deepakjha1104@gmail.com>
2023-09-17 23:05:09 +0530

    new commit

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
-const mongoURI = "mongodb://0.0.0.0:27017/iNoteBook"
+const mongoURI = "mongodb+srv://abc:abc@cluster0.e1y5hap.mongodb.net/iNotebook"
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () =>{
         console.log("Connected to mongo succesfully");
